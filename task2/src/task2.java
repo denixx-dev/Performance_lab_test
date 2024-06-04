085c9da559a35f4ae8cfe32f19bbcd0b08e29609
denixx-dev <denissm2002@yandex.ru>
2024-06-04 15:11:00 +0300

    fixed input in task2

--- task2/src/task2.java
+++ task2/src/task2.java
@@ -7,8 +7,12 @@ import java.util.*;
 
 public class task2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader1 = new BufferedReader(new FileReader("file1.txt"));
-        Scanner scanner = new Scanner(new File("file2.txt"));
+
+        String file1 = args[0];
+        String file2 = args[1];
+
+        BufferedReader reader1 = new BufferedReader(new FileReader(file1));
+        Scanner scanner = new Scanner(new File(file2));
 
         int[] coordinates = new int[2];
         int radius;
